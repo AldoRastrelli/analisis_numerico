073f9dfe9a3d7e7b4e4c77be4f591c285e38d3e1
unknown <arastrelli@produccion.gob.ar>
2021-05-20 20:15:05 -0300

    comentarios y notas

--- metodos.py
+++ metodos.py
@@ -2,9 +2,8 @@ import numpy as np
 from sympy import *
 import math
 
-
 # item 2 a
-# Programar un algoritmo para aproximar π utilizando la funci´on seno(x) con el m´etodo de
+# Programar un algoritmo para aproximar π utilizando la función seno(x) con el m´etodo de
 # Newton-Raphson, en función de x, que realice iteraciones hasta alcanzar el l´ımite de la herramienta utilizada.
 
 def newton_raphson_wrapper_getPi(tolerancia, maxIteraciones, semilla):
@@ -12,13 +11,12 @@ def newton_raphson_wrapper_getPi(tolerancia, maxIteraciones, semilla):
     seno = math.sin
     seno_derivado = math.cos
 
-
     historia = np.zeros((maxIteraciones, 3)) # matriz maxIteraciones x 3
     if tolerancia < 0 or maxIteraciones < 0:
         print(" El intervalo no contiene información suficiente: no puede asegurarse una raíz")
         return None, np.array([])
 
-    return NewtonRaphsonRecursivo(seno, seno_derivado, tolerancia, maxIteraciones, semilla, 0, historia)
+    return newton_raphson(seno, seno_derivado, tolerancia, maxIteraciones, semilla, 0, historia)
 
 
 def newton_raphson(funcion, funcion_derivada, tolerancia, maxIteraciones, x_n, iteracion, historia):
@@ -29,8 +27,8 @@ def newton_raphson(funcion, funcion_derivada, tolerancia, maxIteraciones, x_n, i
     if iteracion >= maxIteraciones - 1:
         return None, np.array([])
 
-    valorFuncion = funcion(x_n)
-    valorDerivada = funcion_derivada(x_n)
+    valorFuncion = funcion(x_n) # Evalua sen(x_n)
+    valorDerivada = funcion_derivada(x_n) # Evalua cos(x_n)
 
     if valorDerivada == 0:
         return None, np.array([])
@@ -43,14 +41,14 @@ def newton_raphson(funcion, funcion_derivada, tolerancia, maxIteraciones, x_n, i
 
     if x_n_mas_1 == x_n:
         historia = historia[:iteracion + 1]
-        return x_n, historia
+        return x_n, historia  # Verificar si se devuelve directo x_n
 
     if error < tolerancia:
         historia[iteracion + 1] = (iteracion + 1, x_n_mas_1,error)
         historia = historia[:iteracion + 2]
         return x_n_mas_1, historia
 
-    return NewtonRaphsonRecursivo(funcion, funcion_derivada, tolerancia, maxIteraciones, x_n_mas_1, iteracion + 1, historia)
+    return newton_raphson(funcion, funcion_derivada, tolerancia, maxIteraciones, x_n_mas_1, iteracion + 1, historia)
 
 #item 2 b
 # Programar un algoritmo para aproximar π utilizando la serie de Leibniz, en función de n.
@@ -65,8 +63,8 @@ def serie_leibniz_pi(iteraciones):
     resultado = 0.0
     signo = 1.0
     
-    for i in range(iteraciones):
-        resultado += signo/(2.0*n+1.0)
+    for i in range(iteraciones):   # i = 0, 1, 2, 3..
+        resultado += signo/(2.0*i+1.0)
         signo = -signo
 
     return 4*resultado
@@ -77,18 +75,27 @@ def serie_leibniz_pi(iteraciones):
 # n = 100000.
 
 def pruebas_newton_raphson_32():
+    # newton_raphson_wrapper_getPi(tolerancia, maxIteraciones, semilla)
+    # n = maxIteraciones
+    # tolerancia = ?
+    # semilla = ?
+
     # ejecutar newton_raphson para n = 10
     # ejecutar newton_raphson para n = 100
     # ejecutar newton_raphson para n = 1000
     # ejecutar newton_raphson para n = 10000
     # ejecutar newton_raphson para n = 100000
+    return
 
 def pruebas_leibniz_32():
+    #serie_leibniz_pi(iteraciones)
     #ejecutar leibniz para n = 10
     #ejecutar leibniz para n = 100
     #ejecutar leibniz para n = 1000
     #ejecutar leibniz para n = 10000
     #ejecutar leibniz para n = 100000
+    return
+
 
 # item 2 d
 # Ejecutar los programas solicitados en a y b utilizando representación de punto flotante de
@@ -101,6 +108,8 @@ def pruebas_newton_raphson_64():
     # ejecutar newton_raphson para n = 1000
     # ejecutar newton_raphson para n = 10000
     # ejecutar newton_raphson para n = 100000
+    return
+
 
 def pruebas_leibniz_64():
     #ejecutar leibniz para n = 10
@@ -108,22 +117,26 @@ def pruebas_leibniz_64():
     #ejecutar leibniz para n = 1000
     #ejecutar leibniz para n = 10000
     #ejecutar leibniz para n = 100000
+    return
+
 
 # item 2 e
 # Ejecutar los programas solicitados en a y b con una calculadora (aclarar marca y modelo) y
 # comparar las respuestas obtenidas con n = 10, n = 100, n = 1000, n = 10000 y n = 100000
-# (en caso de no alcanzar la memoria de la calculadora utilizar el m´aximo n posible).
+# (en caso de no alcanzar la memoria de la calculadora utilizar el máximo n posible).
 
-    """Hacer las cuentas"""
+    """Hacer las cuentas""" ## Aldi
 
 # item 2 f
-# Representar las dos respuestas finales obtenidas (para n = 100000 y el método de Newton
+# Representar las dos respuestas finales obtenidas (para n = 100,000 y el método de Newton
 # Raphson) en c, d y e de manera de expresarlo como π = π(vect) + ∆π
 
     """ calcular ∆π """
+    # | Xn -  X(n+1) |
 
 # item 2 g
-# Podemos afirmar qué para la computadora el número π es una constante?
+# Podemos afirmar que para la computadora el número π es una constante?
 
-    """En la computadora, el número π es una constante, porque es una representación del número π con la cantidad de decimales que la representación del sistema permita"""
+    """En la computadora, el número π no es una constante porque se contempla como una variable con error aproximado de 1*e^(-15).
+    Al mismo tiempo, dependerá de la cantidad de bits disponibles para su representación."""
 
